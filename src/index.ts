d7f17a56fda66ef33183ef85620a34ab2d40ca9f
alirni <aliramezani19.1@gmail.com>
2020-12-26 22:22:50 +0330

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const port = 3030
+const port = 3030;
 
 app.get('/test', (req, res) => {
   res.send('Hello World!')
 });
 
-app.post('/', (req, res) => {
-  res.send('Got a POST request')
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
 });
 
 app.put('/user', (req, res) => {
   res.send('Got a PUT request at /user')
-});
+})
 
 app.delete('/user', (req, res) => {
   res.send('Got a DELETE request at /user')

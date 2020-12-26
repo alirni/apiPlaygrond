f113c5de69bc9f22e76a56beb793479938904c44
alirni <aliramezani19.1@gmail.com>
2020-12-26 20:40:14 +0330

    fix(app): REST

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const app = express()
 const port = 3030
 
 app.get('/test', (req, res) => {
-  console.log('index.js:6 ????', {req});
   res.send('Hello World!')
-})
+});
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+});
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user')
+});
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
